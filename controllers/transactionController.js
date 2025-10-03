3c1c5ee785bdec7b3cbea0e0077da97f06485e51
boukadia <mustapha.boukadia97@gmail.com>
2025-10-03 11:35:18 +0100

    modifier le form pour pouvoir ajoutr titre du savinggoals

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,134 +1,143 @@
 // const { Transaction, User, Category } = require('../models');
 const session = require("express-session");
-const {User}=require('../models')
-const {Category}=require('../models')
-const {Transaction}=require('../models');
+const { User } = require("../models");
+const { Category } = require("../models");
+const { Transaction } = require("../models");
+const {SavingGoal}=require("../models")
 const { where } = require("sequelize");
-exports.getTransactions= async (req,res) =>{
-  
+exports.getTransactions = async (req, res) => {
   try {
     const userId = req.session.userId;
-    
+
     if (!userId) {
-      return res.status(401).send('Unauthorized');
+      return res.status(401).send("Unauthorized");
     }
-    const transactions=await Transaction.findAll({where : {userId}
-    ,include:[Category]
-  });
-  const categories=await Category.findAll();
-  res.render("transactions" ,{ transactions,categories} )
-
-  } catch (error) {
-    
-  }
-}
-exports.createTransaction= async(req,res)=>{
- 
-
+    const transactions = await Transaction.findAll({
+      where: { userId },
+      include: [Category],
+    });
+    const categories = await Category.findAll();
+    res.render("transactions", { transactions, categories });
+  } catch (error) {}
+};
+exports.createTransaction = async (req, res) => {
   try {
-  const category=await Category.findOne({where:{id:req.body.categoryId}})
-    
- const userId=req.session.userId;
-  if (!userId) {
-      return res.status(401).send('Unauthorized');
+    const category = await Category.findOne({
+      where: { id: req.body.categoryId },
+    });
+
+    const userId = req.session.userId;
+    if (!userId) {
+      return res.status(401).send("Unauthorized");
     }
-  
-    if (category.name==="salaire" && req.body.type==="revenu" ) {
-      await User.update({salaire:req.body.montant},{where:{id:userId}})
-       await User.increment({ solde: req.body.montant }, { where: { id: userId } });
+
+    if (category.name === "salaire" && req.body.type === "revenu") {
+      await User.update(
+        { salaire: req.body.montant },
+        { where: { id: userId } }
+      );
+      await User.increment(
+        { solde: req.body.montant },
+        { where: { id: userId } }
+      );
+    } else if (req.body.type === "revenu") {
+      await User.increment(
+        { solde: req.body.montant },
+        { where: { id: userId } }
+      );
+    } else {
+      const user = await User.findOne({ where: { id: userId } });
+      if (user.solde < req.body.montant) {
+        return res.redirect(
+          `/transactions?error=solde_insuffisant&solde=${user.solde}`
+        );
+      }
+
+      await User.decrement(
+        { solde: req.body.montant },
+        { where: { id: userId } }
+      );
+    }
+
+
+    // =====================DEBUT: savingGOALS==========================
+    if (req.body.type="saving") {
       
+      const savingGoal=await SavingGoal.create({})
     }
-    else if (req.body.type==='revenu' ){ 
-      await User.increment({ solde: req.body.montant }, { where: { id: userId } });
-    }
-    else  {  
-      const user = await User.findOne({where:{id:userId}});
-      if(user.solde < req.body.montant){
-        return res.redirect(`/transactions?error=solde_insuffisant&solde=${user.solde}`);
-      }
 
-      await User.decrement({ solde: req.body.montant }, { where: { id: userId } });
-    }
-    
-  const {montant,date,description,categoryId,type}=req.body;
-    await Transaction.create(
-      {
-        montant,
-        description,
-        userId,
-        categoryId,
-        date,
-        type
 
-      }
-    )
-    res.redirect('/transactions');
+    // ========================FIN: savingGoals========================
+
+    const { montant, date, description, categoryId, type } = req.body;
+    await Transaction.create({
+      montant,
+      description,
+      userId,
+      categoryId,
+      date,
+      type,
+    });
+    res.redirect("/transactions");
   } catch (error) {
     res.status(500).send("Error creating transaction: " + error.message);
   }
+};
 
-}
+exports.editTransaction = async (req, res) => {
+  console.log("is", req.params.id);
 
-exports.editTransaction= async(req,res)=>{
-  console.log("is",req.params.id);
-   
   // const transactionId = req.params.id;
-}
-
+};
 
 // module.exports = transactionController;
-exports.updateTransaction=async(req,res)=>{
+exports.updateTransaction = async (req, res) => {
   try {
     const transactionId = req.params.id;
-    const transaction =await Transaction.findOne({where:{id:req.params.id}})
-    const user=await User.findOne({where:{id:req.session.userId}})
-    if (transaction.type==="depense"|| transaction.type==="saving") {
-      user.update({solde:user.solde+transaction.montant}) //travail avec const user
+    const transaction = await Transaction.findOne({
+      where: { id: req.params.id },
+    });
+    const user = await User.findOne({ where: { id: req.session.userId } });
+    if (transaction.type === "depense" || transaction.type === "saving") {
+      user.update({ solde: user.solde + transaction.montant }); //travail avec const user
 
-      await Transaction.update(req.body,{where:{id:transactionId}
-    
-    })
-    user.update({solde:user.solde-req.body.montant}) //travail avec const user
+      await Transaction.update(req.body, { where: { id: transactionId } });
+      user.update({ solde: user.solde - req.body.montant }); //travail avec const user
     } else {
-      user.update({solde:user.solde-transaction.montant}) //travail avec const user
+      user.update({ solde: user.solde - transaction.montant }); //travail avec const user
 
-      await Transaction.update(req.body,{where:{id:transactionId}
-    
-    })
-    user.update({solde:user.solde+Number(req.body.montant)}) //travail avec const user
+      await Transaction.update(req.body, { where: { id: transactionId } });
+      user.update({ solde: user.solde + Number(req.body.montant),salaire:req.body.montant }); //travail avec const user
     }
-    
-    
-      res.redirect('/transactions')
 
+    res.redirect("/transactions");
   } catch (error) {
     res.status(500).send("Error updating transaction: " + error.message);
   }
-  
-}
-exports.deleteTransaction=async(req,res)=>{
+};
+exports.deleteTransaction = async (req, res) => {
   try {
-    const user =await User.findOne({where:{id:req.session.userId}})
-    let solde=user.solde;
+    const user = await User.findOne({ where: { id: req.session.userId } });
+    let solde = user.solde;
     const transactionId = req.params.id;
-    const transaction =await Transaction.findOne({where:{id:req.params.id}})
-if (transaction.type==="depense"|| transaction.type==="saving") {
-    await User.increment({solde:transaction.montant},{where:{id:req.session.userId}}) //travail avec le model direct (solde/usserId)
-    await Transaction.destroy({where:{id:transactionId}
-    })
-    res.redirect('/transactions')
-} else {
-  
-  solde-=transaction.montant;
-  await user.update({solde}) //travail avec const user
-  
-    await Transaction.destroy({where:{id:transactionId}})
-    res.redirect("/transactions")
+    const transaction = await Transaction.findOne({
+      where: { id: req.params.id },
+    });
+    if (transaction.type === "depense" || transaction.type === "saving") {
+      await User.increment(
+        { solde: transaction.montant },
+        { where: { id: req.session.userId } }
+      ); //travail avec le model direct (solde/usserId)
+      await Transaction.destroy({ where: { id: transactionId } });
+      res.redirect("/transactions");
+    } else {
+      solde -= transaction.montant;
+      await user.update({ solde }); //travail avec const user
 
-}    
-   
-
+      await Transaction.destroy({ where: { id: transactionId } });
+      res.redirect("/transactions");
+    }
   } catch (error) {
     res.status(500).send("Error deleting transaction: " + error.message);
   }
-}
+};
